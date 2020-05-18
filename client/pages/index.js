c8a19cabba9d2e8a36228fa75fcd3a5a51c11fa2
Stanley Huang <infamous_godhand@yahoo.com>
2020-05-18 13:57:08 +1000

    added get token method in client

---
+++ client/pages/index.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link.js";
+import axios from "axios";
+import { Cookies } from "react-cookie";
+
+const serverUrl = "http://localhost:3001";
+
+const cookies = new Cookies();
+
+const Index = () => {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    setToken(cookies.get("token") || null);
+  }, []);
+  const onLoginClick = async () => {
+    console.log("Login called");
+    const response = await axios.get(serverUrl + "/api/login");
+    const token = response.data.token;
+    cookies.set("token", token);
+    setToken({
+      tokentoken,
+    });
+  };
+  return (
+    <div>
+      <h2>Main page</h2>
+      <br></br>
+      <button onClick={onLoginClick}>Get Token</button>
+      <br></br>
+      <p>Token: {token}</p>
+      <br></br>
+      <Link href="/secret">
+        <a>Secret page</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Index;
